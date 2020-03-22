669ff819c4fadb284ff192443bf3203415e78e61
Genericsrush <macray607@hotmail.com>
2020-03-21 20:22:56 -0500

    Initial setup

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,6 @@
 class Customer < ApplicationRecord
+
+  validates :full_name, presence: true
+
+  has_one_attached :face
 end
--- db/migrate/20200322003058_create_customers.rb
+++ db/migrate/20200322003058_create_customers.rb
@@ -1,12 +1,13 @@
 class CreateCustomers < ActiveRecord::Migration[6.0]
   def change
     create_table :customers do |t|
-      t.text :full_name
-      t.text :phone_number
-      t.text :email_address
+      t.string :full_name
+      t.string :phone_number
+      t.string :email_address
       t.text :notes
 
       t.timestamps
     end
   end
 end
+git
